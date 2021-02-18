e1a45326bf3a707bc9f713285e58d0d00f62e543
JUAN ZAPATA <jzapata@overactive.com>
2021-02-18 09:43:28 -0300

    https

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://pautarosario12.herokuapp.com/api",
+  baseURL: "https://pautarosario12.herokuapp.com/api",
   headers: {
     "Content-type": "application/json"
   }
