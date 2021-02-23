b003b91ceb1337043ec7ff8b4fdefff7cf1fed16
JUAN ZAPATA <jzapata@overactive.com>
2021-02-23 12:55:20 -0300

    Client Detail new UI

--- src/components/Client.vue
+++ src/components/Client.vue
@@ -1,115 +1,127 @@
 <template>
-  <div v-if="currentClient" class="edit-form">
-    <sui-form>
-      <sui-header dividing>Editar Cliente</sui-header>
-      <sui-form-field>
-        <label>Nombre</label>
-        <input placeholder="Nombre" type="text" 
-          required
-          v-model="currentClient.nombre" />
-      </sui-form-field>
-
-      <sui-form-field>
-        <label>CUIT</label>
-        <input placeholder="CUIT" type="text" 
-          required
-          v-model="currentClient.cuit" />
-      </sui-form-field>
-
-      <sui-form-field>
-        <sui-checkbox label="Sujeto exento" required toggle v-model="currentClient.sujetoExento" />
-      </sui-form-field>
-
-      <sui-form-field>
-        <label>Direccion</label>
-        <input placeholder="Direccion" type="text" 
-          required
-          v-model="currentClient.dir" />
-      </sui-form-field>
-
-      <sui-form-field>
-        <label>Telefono</label>
-        <input placeholder="Telefono" type="text" 
-          required
-          v-model="currentClient.tel" />
-      </sui-form-field>
-
-      <sui-form-field>
-        <label>Mail</label>
-        <input placeholder="Mail" type="text" 
-          required
-          v-model="currentClient.mail" />
-      </sui-form-field>
-    </sui-form>
-    <sui-divider />
-    <sui-button  content="Borrar"  @click="deleteClient" />
-    
-    <sui-button  content="Actualizar"  @click="updateClient" />
-    
-    <p>{{ message }}</p>
-  </div>
-
-  <div v-else>
-    <br />
-    <p>Please click on a Client...</p>
-  </div>
+    <b-form @submit.prevent="updateClient">
+        <h4>Editar cliente</h4>
+        <b-form-group
+                id="input-group-1"
+                label-for="input-1"
+                label="Nombre" 
+            >
+                <b-form-input
+                    id="input-1"
+                    v-model="client.nombre"
+                    placeholder="Nombre"
+                    required
+                    ></b-form-input>
+            </b-form-group>
+            <b-form-group
+                id="input-group-2"
+                label-for="input-2"
+                label="CUIT"
+            >
+                <b-form-input
+                    id="input-2"
+                    v-model="client.cuit"
+                    placeholder="CUIT"
+                    required
+                    ></b-form-input>
+            </b-form-group>
+            <b-form-group
+                id="input-group-3"
+                label-for="input-3"
+                label="Sujeto Exento"
+                content-cols-sm
+            >
+                <b-form-checkbox id="input-3" v-model="client.sujetoExento" 
+                    value="true"
+                    unchecked-value="false"
+                ></b-form-checkbox>
+            </b-form-group>
+            <b-form-group
+                id="input-group-4"
+                label-for="input-4"
+                label="Direccion"
+            >
+                <b-form-input
+                    id="input-4"
+                    v-model="client.dir"
+                    required
+                    ></b-form-input>
+            </b-form-group>
+            <b-form-group
+                id="input-group-5"
+                label-for="input-5"
+                label="Telefono"
+            >
+                <b-form-input 
+                    id="input-5"
+                    v-model="client.tel"
+                    required
+                    ></b-form-input>
+            </b-form-group>
+            <b-form-group
+                id="input-group-6"
+                label-for="input-6"
+                label="Mail"
+            >
+                <b-form-input
+                    type="email"
+                    id="input-6"
+                    v-model="client.mail"
+                    required
+                    ></b-form-input>
+            </b-form-group>
+            <b-button-group>
+                <b-button type="submit" variant="primary">Guardar</b-button>
+                <b-button  variant="danger" @click="deleteClient">Borrar</b-button>
+            </b-button-group>
+    </b-form>
 </template>
 
 <script>
 import ClientDataService from "../services/ClientDataService";
-
 export default {
-  name: "client",
-  data() {
-    return {
-      currentClient: null,
-      message: ''
-    };
-  },
-  methods: {
-    getClient(id) {
-      ClientDataService.get(id)
-        .then(response => {
-          this.currentClient = response.data;
-          console.log(response.data);
-        })
-        .catch(e => {
-          console.log(e);
-        });
+    name: "client",
+    data() {
+        return {
+            client: null
+        }
     },
+    methods: {
+        getClient(id) {
+            ClientDataService.get(id)
+            .then(response => {
+                this.client = response.data;
+                console.log(response.data);
+            })
+            .catch(e => {
+                console.log(e);
+            });
+        },
 
-    updateClient() {
-      ClientDataService.update(this.currentClient._id, this.currentClient)
-        .then(response => {
-          console.log(response.data);
-          this.message = 'The client was updated successfully!';
-        })
-        .catch(e => {
-          console.log(e);
-        });
+        updateClient() {
+            ClientDataService.update(this.client._id, this.client)
+                .then(response => {
+                    console.log(response)
+                    this.$router.push({ name: "clients" });
+                })
+                .catch(e => {
+                    console.log(e);
+                });
+        },
+
+        deleteClient() {
+            ClientDataService.delete(this.client._id)
+                .then(response => {
+                    console.log(response)
+                    this.$router.push({ name: "clients" });
+                })
+                .catch(e => {
+                    console.log(e);
+                });
+        }
     },
-
-    deleteClient() {
-      ClientDataService.delete(this.currentClient._id)
-        .then(response => {
-          console.log(response.data);
-          this.$router.push({ name: "clients" });
-        })
-        .catch(e => {
-          console.log(e);
-        });
-    }
-  },
   mounted() {
-    this.message = '';
     this.getClient(this.$route.params.id);
   }
-};
-</script>
-
-<style>
-.edit-form {
-  max-width: 300px;
-  margin: auto;
 }
-</style>
+</script>
